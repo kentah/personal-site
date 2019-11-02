e9c8ed8f96dddf7690bd819b0de466ecccbd6153
kenthow@gmail.com <kenthow@gmail.com>
2019-11-02 17:35:30 -0400

    added axios for blog. Rendered posts into /blog from express via axios

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,15 +1,62 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
+var PORT = 9000;
 
 class Blog extends Component {
+    // data:
+    //  objId:
+    //      id:
+    //      title:
+    //      content:
+    //      createdAt:
+    //      updatedAt:
+    //      userId
+
+    state = {
+        posts: [],
+        isLoading: true,
+        errors: null
+    };
+
+    getPosts() {
+        axios.get(`http://localhost:${PORT}/posts`)
+            .then(res => {
+                this.setState({
+                    posts: res.data,
+                    isLoading: false
+                });
+            })
+            .catch(err => this.setState({ err, isLoading: false}));
+    };
+
+    componentDidMount() {
+        this.getPosts()
+    }
+
     render() {
+        const { isLoading, posts } = this.state;
         return(
-            <div>
-                Blog
+            <div className='blog'>
+                {!isLoading ? (
+                    posts.map(post => {
+                        const {id, title, content, createdAt } = post;
+                        return (
+                            <div key={id}>
+                                <article className='pa3 pa5-ns'>
+                                    <h1 classNem='f3 fl-m f-headline-l'>{title}</h1>
+                                    <h5>{createdAt}</h5>
+                                    <p className='measure lh-copy'>{content}</p>
+                                </article>
+                            </div>
+                        );
+                    })
+                ) : (
+                    <p>Loading...</p>
+                )}
             </div>
         );
     }
-
 };
 
 export default Blog;
